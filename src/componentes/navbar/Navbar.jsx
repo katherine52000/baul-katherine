c498958726c1f16cc8987a306e6706c0025317a9
katherine52000 <kath52000@outlook.es>
2022-07-13 20:10:35 -0500

    navbar

---
+++ src/componentes/navbar/Navbar.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const navbar = () => {
+  return (
+    <div>
+<button className='Navbar_button_menu'>MENU</button>
+<button className='Navbar_button_Favoritos'>FAVORITOS</button>
+<button className='Navbar_button_LoMasVedido'>LO MAS VENDIDO</button>
+<button className='Navbar_button_contacto'>Contacto</button>
+    </div>
+  )
+}
+
+export default navbar
